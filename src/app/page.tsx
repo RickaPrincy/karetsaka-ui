af2df482992a7368c159693fafc7fd7c829bdf5f
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-05-28 18:15:30 +0000

    refactor: restructure project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import { BrandCard, LandingPage } from "@/components";
+import {
+  CarBrand,
+  LandingPage,
+} from "@/operations/front-office/home/components";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
       <LandingPage />
       <div className="mt-12 padding-x padding-y max-width">
-        <BrandCard />
+        <CarBrand />
       </div>
     </div>
   );
