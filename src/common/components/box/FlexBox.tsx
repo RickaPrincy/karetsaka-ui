855fbffc522b912832bca44375d2b723d2e839ac
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-05-29 08:58:15 +0000

    feat: login design
    * feat: login design
    
    * fix(build): FLEX_BOX_SX type

---
+++ src/common/components/box/FlexBox.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { Box as MuiBox, BoxProps } from "@mui/material";
+import { FLEX_BOX_SX } from "@/common/utils/common-props";
+
+export const FlexBox: FC<BoxProps> = ({ sx = {}, ...boxProps }) => {
+  return (
+    <MuiBox sx={{ ...FLEX_BOX_SX, ...sx } as BoxProps["sx"]} {...boxProps} />
+  );
+};
---
+++ src/common/utils/palette-colors.ts
@@ -0,0 +1,11 @@
+export const PALETTE_COLORS = {
+  blue: {
+    "1": "#4873d9",
+  },
+  gray: {
+    "1": "#515254",
+  },
+  black: {
+    "1": "#4f4e4c",
+  },
+};
