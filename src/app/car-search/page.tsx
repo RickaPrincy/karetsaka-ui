6e1dc3826ce8d14c7a3fbe77cb1cf71debb8d7cd
Fifaliana <hei.fifaliana@gmail.com>
2024-05-30 13:45:15 +0300

    feat: a page for car search

---
+++ src/app/car-search/page.tsx
@@ -0,0 +1,13 @@
+import {
+  CarModel,
+  CarSearch,
+} from "@/operations/front-office/car-search/components";
+
+export default function Car() {
+  return (
+    <>
+      <CarSearch />
+      <CarModel />
+    </>
+  );
+}
