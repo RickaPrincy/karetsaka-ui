0216a9ebf8c4e9517f6ede71b904d4a69894a556
Fifaliana <hei.fifaliana@gmail.com>
2024-05-30 13:44:10 +0300

    feat: add car search into the landing page

--- src/operations/front-office/home/components/landing-page.tsx
+++ src/operations/front-office/home/components/landing-page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import golfImage from "@/assets/images/golf.png";
+import { CarSearch } from "../../car-search/components";
 
 export function LandingPage() {
   return (
@@ -7,6 +8,9 @@ export function LandingPage() {
       <div className="flex-1 pt-36 padding-x">
         <h1 className="landing__title">Welcome to karetsaka</h1>
         <p className="landing__subtitle">Find your dream car in karetsaka</p>
+        <div className="landing__search">
+          <CarSearch />
+        </div>
       </div>
       <div className="landing__image-container">
         <div className="landing__image">
