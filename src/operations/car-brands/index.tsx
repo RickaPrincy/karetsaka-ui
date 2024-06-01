faa59423593f281f053ea5c912d52843968a1893
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-06-01 16:31:48 +0300

    feat: car brands
    Co-authored-by: sanda03 <hei.sanda.2@gmail.com>

---
+++ src/operations/car-brands/index.tsx
@@ -0,0 +1,7 @@
+import { CarBrandCreate } from "./car-brand-create";
+import { CarBrandList } from "./car-brand-list";
+
+export const CAR_BRAND_UI = {
+  create: CarBrandCreate,
+  list: CarBrandList,
+};
--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,6 +1,7 @@
-import { HealthApi, SecurityApi, UsersApi } from "../gen/client";
+import { CarsApi, HealthApi, SecurityApi, UsersApi } from "../gen/client";
 import { getCachedConfiguration } from "./utils";
 
 export const healthApi = () => new HealthApi(getCachedConfiguration());
 export const securityApi = () => new SecurityApi(getCachedConfiguration());
 export const usersApi = () => new UsersApi(getCachedConfiguration());
+export const carsApi = () => new CarsApi(getCachedConfiguration());
