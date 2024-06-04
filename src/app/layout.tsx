2586be5b2ba0018d4aad8c82f28228af83dd9eb5
Fifaliana <hei.fifaliana@gmail.com>
2024-06-05 00:44:44 +0300

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ResponsiveAppBar } from "@/operations/front-office/components";
 
 export const metadata: Metadata = {
   title: "Karetsaka",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ResponsiveAppBar />
+        {children}
+      </body>
     </html>
   );
 }
