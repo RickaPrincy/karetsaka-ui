f5187a935e0e3e69a2f20a12336ae5ba1535ea8e
Fifaliana <hei.fifaliana@gmail.com>
2024-06-07 22:22:04 +0300

    chore: add button link to login

--- src/operations/front-office/components/navbar.tsx
+++ src/operations/front-office/components/navbar.tsx
@@ -1,18 +1,6 @@
 "use client";
 import * as React from "react";
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
-  Container,
-  Avatar,
-  Button,
-  Tooltip,
-  MenuItem,
-} from "@mui/material";
+import { AppBar, Container, Box, Toolbar, Button } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,28 +9,8 @@ const pages = [
   { label: "Car", href: "/car" },
   { label: "Appointment", href: "/appointment" },
 ];
-const settings = ["Dashboard"];
 
 export function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar position="static" sx={{ backgroundColor: "#4DBCE7" }}>
       <Container maxWidth="xl">
@@ -60,44 +28,18 @@ export function ResponsiveAppBar() {
           >
             {pages.map((page) => (
               <Link key={page.label} href={page.href} passHref>
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
                   {page.label}
                 </Button>
               </Link>
             ))}
           </Box>
-
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            <Link href="/admin" passHref>
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </Link>
           </Box>
         </Toolbar>
       </Container>
