35fa0fee4acc7ad7fcbb1f24a731a94e9cc368a0
RickaPrincy <rckprincy@gmail.com>
2024-06-08 04:35:55 +0300

    feat: footer

--- src/app/client/layout.tsx
+++ src/app/client/layout.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { ResponsiveAppBar } from "@/operations/front-office/components";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Box, Typography } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+import { FlexBox } from "@/common/components/box";
+import { PALETTE_COLORS } from "@/common/utils/palette-colors";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +23,29 @@ export default function Layout({
       <QueryClientProvider client={queryClient}>
         <ResponsiveAppBar />
         {children}
+        <FlexBox sx={{ width: "100%", p: 5, bgcolor: "#06022e", mt: 5 }}>
+          <Box sx={{ flex: 1 }}>
+            <Typography
+              variant="h1"
+              sx={{ fontSize: "25px", color: "white", textAlign: "center" }}
+            >
+              <span
+                style={{
+                  color: PALETTE_COLORS.orange["1"],
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                }}
+              >
+                K
+              </span>
+              aretsaka
+            </Typography>
+            <Typography sx={{ color: "white", textAlign: "center" }}>
+              Made by <Favorite color="warning" />{" "}
+              <span style={{ fontWeight: "bold" }}>Karetsaka team</span>
+            </Typography>
+          </Box>
+        </FlexBox>
       </QueryClientProvider>
     </div>
   );
